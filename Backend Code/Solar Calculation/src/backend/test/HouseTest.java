856d776468eacf7253188b5de9441e440f996277
JamesFlood <jDingo@hotmail.com>
2012-09-23 19:12:55 +1000

    house tests

--- Backend Code/Solar Calculation/src/backend/test/HouseTest.java
+++ Backend Code/Solar Calculation/src/backend/test/HouseTest.java
@@ -14,8 +14,7 @@ public class HouseTest {
 	@Before
 	public void setUp(){
 		
-		House1 = new House();
-		House1.setLocation("Australia");
+		House1 = new House("Australia", "AUD", 100.0, 50.0, 12.5, "Energex", 0.5, 19000.0);
 		
 	}
 	
@@ -24,6 +23,43 @@ public class HouseTest {
 	public void getCountry(){
 		assertTrue(House1.getCountry()=="Australia");
 	}
+	
+	@Test
+	public void getCurency(){
+		assertTrue(House1.getCurrency()=="AUD");
+	}
+	
+	@Test
+	public void getLat(){
+		assertTrue(House1.getLat()==100.0);
+	}
+	
+	@Test
+	public void getLon(){
+		assertTrue(House1.getLon()==50.0);
+	}
+	
+	@Test
+	public void getWire(){
+		assertTrue(House1.getWire()==12.5);
+	}
+	
+	@Test
+	public void getComapny(){
+		assertTrue(House1.getElectricityCompany()=="Energex");
+	}
+	
+	@Test
+	public void getTariff(){
+		assertTrue(House1.getTariff()==0.5);
+	}
+	
+	@Test
+	public void getCost(){
+		assertTrue(House1.getCost()==19000.0);
+	}
+	
+	
 	
 	
 	
